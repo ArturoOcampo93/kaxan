3ff1de4c1ca90f8a284bd4e98c2884ad23f13448
AOcampo93 <54147798+ArturoOcampo93@users.noreply.github.com>
2022-06-27 20:42:38 -0500

    Ajuste Foto de Perfil Kaxan
    + Se añadio un icono de "Editar Foto" en la sección del dashboard
    + Se añadio el nuevo circulo de perfil en el header
    +Ajustes de estilos generales (Header y Dasboard)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -19,6 +19,52 @@ declare var $: any;
 
 
 export class HeaderComponent implements OnInit {
+
+
+
+    // constructor() { }
+    // miseccion: number;
+    // vistaUsuario: any;
+    // porcentaje: number = 25;
+    // ngOnInit(): void {
+      
+    //   this.miseccion = 0
+    //   this.vistaUsuario = JSON.parse(localStorage.getItem("datosKaxan"))
+    //   //console.log(this.vistaUsuario);
+    //   this.valorInput();
+    // }
+  
+    // cambiaSeccion(seccion){
+    //   this.miseccion = seccion
+    // }
+  
+    // valorInput(){
+    //   let val = this.porcentaje;
+    //   console.log('Valor input', val);
+    //   var $circle = $('#svg2 #bar2');
+  
+    //   if (isNaN(val)) {
+    //       val = 100;
+    //   } else {
+    //       var r = $circle.attr('r');
+    //       var c = Math.PI * (r * 2);
+  
+    //       if (val < 0) { val = 0; }
+    //       if (val > 100) { val = 100; }
+  
+    //       var pct = ((100 - val) / 100) * c;
+  
+    //       $circle.css({ strokeDashoffset: pct });
+  
+    //       $('#cont2').attr('data-pct', val);
+    //   }
+      
+    // }
+  
+
+
+
+
   @ViewChild('closebutton') closebutton;
 
 
@@ -524,6 +570,8 @@ export class HeaderComponent implements OnInit {
     }
 
   }
+
+  
 
 
   
